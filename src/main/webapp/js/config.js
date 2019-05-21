b513f2b90eb27df3219a3f3b3f7253d97a238771
itmsuk01 <sukiasyan.samvel@it-motive.de>
2019-05-21 11:52:21 +0200

    add libraries and require js

---
+++ src/main/webapp/js/config.js
@@ -0,0 +1,14 @@
+require.config({
+    deps: ["main"],
+    baseUrl: "js",
+    paths: {
+        //Libraries
+        jquery: "libraries/jquery-3.3.1.min",
+        bootstrap: "libraries/bootstrap/bootstrap.min",
+
+        //Directories
+        views: "../views",
+        templates: "../templates"
+    }
+
+});
