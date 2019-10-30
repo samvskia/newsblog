be0a1e4734678f66c2d781970604588dcab72ab9
itmsuk01 <samvskia@gmail.com>
2019-10-30 12:11:45 +0100

    DB changes

--- src/main/java/com/newsblog/database/DBProcessor.java
+++ src/main/java/com/newsblog/database/DBProcessor.java
@@ -12,7 +12,7 @@ public class DBProcessor {
 
     private Connection connection;
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Gaugamela331";
+    private static final String PASSWORD = "123456";
     private static final String URL = "jdbc:mysql://localhost:3306/newsblog?useSSL=false&serverTimezone=UTC";
 
     public DBProcessor() {
